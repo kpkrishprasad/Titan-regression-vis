f4e0b48ae6e5c5f3ee02976b5a0bf56bc177859e
Krish Prasad <krishprasad@Krishs-MacBook-Pro.local>
2024-05-28 19:38:00 -0700

    a

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,18 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
-  base: './',  // Adjust this if deploying to a subpath
+  base: '/Titan-regression-vis/',  // Adjust this if deploying to a subpath, use './' or '/' if at the root
   build: {
     outDir: 'dist',  // Customize the output directory
+    rollupOptions: {
+      output: {
+        // Ensures that assets are placed in a structured way if necessary
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'build/[name].[hash].js',
+      }
+    }
   },
-  // If you have issues with paths, this might be useful
   resolve: {
     alias: {
       '@': '/src',  // This allows you to use '@/somePath' as an alias to '/src/somePath'
